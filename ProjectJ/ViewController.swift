7d35344279e0acc0f79297666467c15844de4f9b
Kenny Qi Yen Kan <kennyqiyenkan@users.noreply.github.com>
2017-10-31 23:04:27 -0400

    Initial Commit

---
+++ ProjectJ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectJ
+//
+//  Created by Kenny Kan on 2017-10-31.
+//  Copyright © 2017 JK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
